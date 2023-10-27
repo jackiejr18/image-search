9a050314b05074a07ffe1fafbea0bd0eb180aede
Jackie Jr <jwysinger@gmail.com>
2023-10-27 12:33:46 -0500

    12 images per page with non functional show more button

--- imagesearch.js
+++ imagesearch.js
@@ -11,7 +11,7 @@ let page = 1;
 
 async function searchImages(){
   keyword = searchBox.value;
-  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}`;
+  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`;
 
   const response = await fetch(url);
   const data = await response.json();
@@ -28,6 +28,8 @@ async function searchImages(){
     imageLink.appendChild(image);
     searchResult.appendChild(imageLink);
   })
+
+  showMoreBtn.style.display = 'block';
 }
 
 searchForm.addEventListener('submit', (e) => {
