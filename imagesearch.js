3b2c307cde38ee382f0fd6b441fe9b7db3c66b69
Jackie Jr <jwysinger@gmail.com>
2023-10-27 12:06:21 -0500

    function that returns links to pictures in console with api

--- imagesearch.js
+++ imagesearch.js
@@ -1,4 +1,26 @@
+const accessKey = 'YywTRhljaGZ2DiNPwNr5nPRY4YQVMVUB9QcmGvzIejY';
+
 const searchForm = document.getElementById('search-form');
 const searchBox = document.getElementById('search-box');
 const searchResult = document.getElementById('search-result');
 const showMoreBtn = document.getElementById('show-more-btn');
+
+
+let keyword = '';
+let page = 1;
+
+async function searchImages(){
+  keyword = searchBox.value;
+  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}`;
+
+  const response = await fetch(url);
+  const data = await response.json();
+
+  console.log(data); 
+}
+
+searchForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  page = 1; 
+  searchImages(); 
+})
